47bb2d7d5e4719524f2e5adf22826656e232a726
ROUSSINAntoine <47926240+ROUSSINAntoine@users.noreply.github.com>
2019-04-26 15:32:55 +0200

    Add files via upload

---
+++ Hocto-doc/README.md
@@ -0,0 +1,1 @@
+# Hocto-doc
---
+++ Hocto-doc/index.php
@@ -0,0 +1,12 @@
+<?php
+    switch(isset($_GET)) {
+        case(isset($_GET['page'])):
+            switch($_GET['page']){
+                default:
+                    include('E/404.html');
+            }
+            break;
+        default:
+            include('C/login.php');
+    }
+?>
