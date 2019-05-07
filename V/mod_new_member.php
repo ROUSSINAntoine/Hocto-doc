6b6028f10eae1df366a15080f52ceec085c7ada3
Antoine ROUSSIN <antoine.roussin.lefevre@gmail.com>
2019-05-07 14:35:13 +0200

    Suite

---
+++ V/mod_new_member.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>New Member</title>
+</head>
+<body>
+    <form action="../index.php" action="GET">
+        <input type="text" name="first_name" placeholder="First name"><br>
+        <input type="text" name="name" placeholder="Name"><br>
+        <input type="text" name="phone_number" placeholder="Phone number"><br>
+        <input type="text" name="address" placeholder="Address"><br>
+        <input type="text" name="city" placeholder="City"><br>
+        <input type="text" name="postcode" placeholder="Postcode"><br>
+        <input type="text" name="ssn" placeholder="Social security number"><br>
+        <input type="hidden" name="page" value="new_member">
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
