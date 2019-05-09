5e7cbb3d0785ac4661f05ebba66399ec7e26b536
dydy92 <dlopes@intechinfo.fr>
2019-05-09 10:17:01 +0200

    ok

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-<?php
-    switch(isset($_GET)) {
-        case(isset($_GET['page'])):
-            switch($_GET['page']){
-                default:
-                    include('E/404.html');
-            }
-            break;
-        default:
-            include('C/login.php');
-    }
-=======
 <?php
     switch(isset($_GET)) {
         case(isset($_GET['page'])):
