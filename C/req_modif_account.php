2344ef763316e0ec718cb2434abbfd389c5970bb
ROUSSIN Antoine <antoine.roussin.lefevre@gmail.com>
2019-05-11 20:19:03 +0200

    fin de répartion de github

---
+++ C/req_modif_account.php
@@ -0,0 +1,13 @@
+<?php
+function modif_psw () 
+{
+    
+    $bdd = new PDO('mysql:host=localhost;dbname=hoctodoc', 'root', '');
+    $reponse =$bdd->prepare ("UPDATE account SET psw =:psw , email =:email WHERE email LIKE :emails");
+    $reponse ->execute (array (
+        "psw"=>$_GET['psw'],
+        "emails"=>$_GET['emails'],
+        "email"=>$_GET['email'] ));
+};
+$reponse = modif_psw();
+?>
