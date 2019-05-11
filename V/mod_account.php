1e064e5c6aeebeff1cf8e713ecf4ca1a656a1fa5
ROUSSIN Antoine <antoine.roussin.lefevre@gmail.com>
2019-05-11 20:15:43 +0200

    bon je refais tout a la main

--- V/mod_account.php
+++ V/mod_account.php
@@ -1,10 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    <a href="../index.php?page=modif_account">modif</a>
-</body>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <title>Account</title>
+    </head>
+    <body>
+        <h1>Your account</h1><p></p>
+        
+        </form>
+        <a href="../index.php?page=modif_account">change your data</a>
+        <a href="../index.php?">add member</a><br>
+        <a href="../index.php?">calendar</a><br>
+        <form action="../index" method="post">
+            <select name="member" id="">
+                <option value="tempo">tempo</option>
+            </select>
+            <input type="hidden" name="page" value="new_member">
+            <input type="submit" value="go">
+        </form>
+        <a href="./home.php"><button>return</button></a>
+    </body>
 </html>
