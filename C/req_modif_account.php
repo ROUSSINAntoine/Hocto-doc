fc7c1784519e39efe496e6eafc0277219699e09e
Antoine ROUSSIN <antoine.roussin.lefevre@gmail.com>
2019-05-12 17:41:04 +0200

    Implémentation de la fonctionalité permettant de changer les données d'un compte

--- C/req_modif_account.php
+++ C/req_modif_account.php
@@ -1,6 +1,5 @@
 <?php
-function modif_psw () 
-{
+function modif () {
     
     $bdd = new PDO('mysql:host=localhost;dbname=hoctodoc', 'root', '');
     $reponse =$bdd->prepare ("UPDATE account SET psw =:psw , email =:email WHERE email LIKE :emails");
@@ -8,23 +7,24 @@ function modif_psw ()
         "psw"=>$_GET['psw'],
         "emails"=>$_GET['emails'],
         "email"=>$_GET['email'] ));
-};
-$reponse = modif_psw();
+}
 
-function affichage_account()
-    {
+function affichage_account() {
     $bdd = new PDO('mysql:host=localhost;dbname=hoctodoc', 'root', '');
 
     $reponse =$bdd->query ("SELECT * FROM account");
 
-    while($donnees = $reponse->fetch()) {
+    $donnees = $reponse->fetch();
     
-        echo $donnees['psw']. '<br/>';
-        echo $donnees['email']. '<br/>';
-
-    }
+    return $donnees;
 }
 
-$reponse = affichage_account();
+if (isset($_GET["psw"])) {
+    modif();
+}
+
+$donnees = affichage_account();
+include("V/mod_modif_account.php");
+
 
 ?>
--- V/mod_modif_account.php
+++ V/mod_modif_account.php
@@ -6,9 +6,11 @@
 <body>
     <form action="index.php" method="GET">
         <input type="text" name="email" value="<?php echo $donnees['email']?>"><br>
-        <input type="text" name="modif_psw" value="<?php echo $donnees['psw']?>"><br>
+        <input type="text" name="psw" value="<?php echo $donnees['psw']?>"><br>
         <input type="hidden" name="page" value="modif_account">
+        <input type="hidden" name="emails" value="<?php echo $donnees['email']?>">
         <input type="submit" value="valider">
     </form>
+    <a href="V/mod_account.php">back</a>
 </body>
 </html>
