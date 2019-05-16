03276f2a8d78567006ff11f9583e25376b1dfbd9
Antoine ROUSSIN <antoine.roussin.lefevre@gmail.com>
2019-05-16 19:57:31 +0200

    prep merge master

--- V/mod_login.php
+++ V/mod_login.php
@@ -18,5 +18,6 @@
             <input type="hidden" name="page" value="login">
             <input type="submit" value="connection">
         </form>
+        <a href="index.php?page=register"><button>register</button></a>
     </body>
 </html>
