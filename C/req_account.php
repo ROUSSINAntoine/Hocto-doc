c8a85069024831e72ab586de881e6c8dbb8234c2
Antoine ROUSSIN <antoine.roussin.lefevre@gmail.com>
2019-05-17 13:37:07 +0200

    bloquage de la modif de données si 0 patient

--- C/req_account.php
+++ C/req_account.php
@@ -2,8 +2,13 @@
     include("M/get_sql.php");
     $req = sql_account();
     $select = "";
+
     while ($data = $req->fetch()) {
         $select = $select."<option value='".$data["id"]."'>".$data["firstname"]." ".$data["lastname"]."</option>\n";
+    }
+
+    if ($select == "") {
+        $dis = "disabled";
     }
     include("V/mod_account.php"); 
 
