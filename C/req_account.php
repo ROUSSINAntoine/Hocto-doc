f9ad63e496e632373e9f9ff68bacbb4c8d16169e
Antoine ROUSSIN <antoine.roussin.lefevre@gmail.com>
2019-05-20 09:09:34 +0200

    regle bug account

--- C/req_account.php
+++ C/req_account.php
@@ -2,6 +2,7 @@
     include("M/get_sql.php");
     $req = sql_account();
     $select = "";
+    $dis = "";
 
     while ($data = $req->fetch()) {
         $select = $select."<option value='".$data["id"]."'>".$data["firstname"]." ".$data["lastname"]."</option>\n";
