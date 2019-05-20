b10e7b7cd662ee421c2592cc147d4db29ba9f744
dydy92 <dlopes@intechinfo.fr>
2019-05-20 09:09:56 +0200

    okidoc

--- V/mod_register.php
+++ V/mod_register.php
@@ -7,8 +7,8 @@
         <h1>Create your account</h1><p></p>
         <form action="index.php" action="GET">
             <input type="text" name="email" placeholder="e-mail"><br>
-            <input type="text" name="pass" placeholder="password"><br>
-            <input type="text" name="conf" placeholder="password"><br>
+            <input type="password" name="pass" placeholder="password"><br>
+            <input type="password" name="conf" placeholder="password"><br>
             
             <input type="hidden" name="page" value="register">
             <input type="submit" name="submit" value="Register">
