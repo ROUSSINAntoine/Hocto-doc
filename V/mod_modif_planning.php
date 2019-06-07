45559d08d180d6b206c53c0d7a96cb9a2831e039
dydy92 <dlopes@intechinfo.fr>
2019-06-07 09:36:31 +0200

    sal

--- V/mod_modif_planning.php
+++ V/mod_modif_planning.php
@@ -17,23 +17,23 @@
     
 <p>
     open
-    <input type="time" name="open_time" min="07:00" max="21:30" required>/><br/>
+    <input type="time" name="open_time" min="07:00" max="21:30" required><br/>
    
     break_time
-    <input type="time" name="break_time" min="11:00" max="21:30" required> /><br/>
+    <input type="time" name="break_time" min="11:00" max="21:30" required><br/>
     resume
-    <input type="time" name="resume_time" min="11:30" max="21:30" required>/><br/>
+    <input type="time" name="resume_time" min="11:30" max="21:30" required><br/>
     fin
-    <input type="time" name="close_time" min="11:00" max="22:30" required> /><br/>
+    <input type="time" name="close_time" min="11:00" max="22:30" required><br/>
     durée
-    <input type="number" name="length_time" min="10" max="120" required>/><br/>
-    days
-    formulaire modif_planning<input type="checkbox" name="lundi" value="lundi" />lundi<br>
+    <input type="number" name="length_time" min="10" max="120" required><br/>
+    days <br>
+   <input type="checkbox" name="lundi" value="lundi" />lundi<br>
    <input type="checkbox" name="mardi" value="mardi" />mardi<br>
    <input type="checkbox" name="mercredi" value="mercredi" />mercredi<br>
    <input type="checkbox" name="jeudi" value="jeudi" />jeudi<br>
    <input type="checkbox" name="vendredi" value="vendredi" />vendredi<br>
-   <input type="checkbox" name="samdedi" value="samedi" />samedi<br>
+   <input type="checkbox" name="samedi" value="samedi" />samedi<br>
     
     début des vacanse
     <input type="date" name="start_hollyday"/><br/>
