55cac141055d8fa325a52c95e5528c3c926cf349
dydy92 <dlopes@intechinfo.fr>
2019-06-17 10:29:40 +0200

    oki

--- C/req_planning.php
+++ C/req_planning.php
@@ -2,13 +2,13 @@
 
 include("M/other_sql.php");
 include("V/mod_créer_planning.php");
-
+if(isset($_GET["d_time"])){
 if($_GET["id='créer-tab'"] = true){
     echo ($_GET["id='mycréerContent'"]);
 } else {
      echo ($_GET["id='mychangeContent'"]);
 }
-
+}
 if(isset($_GET["days_time"])){
 if(isset($_GET["practitioner"])){
 
